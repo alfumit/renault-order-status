cc108386a76f245daa4f2f9c287b057af667c12a
Alexander.G.Fedotov <alexander.g.fedotov@kaspersky.com>
2020-09-08 15:33:03 +0300

    Updates

---
+++ precache-manifest.fcab087c7bc6da4e91e797d876a6279e.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "116b505b1fc0dc24f0cb07189e8b7d3f",
+    "url": "/renault-order-status/index.html"
+  },
+  {
+    "revision": "96116942d96c36c553da",
+    "url": "/renault-order-status/static/css/main.99c7f121.chunk.css"
+  },
+  {
+    "revision": "c3cf6092f52462c25aa1",
+    "url": "/renault-order-status/static/js/2.3f2166db.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/renault-order-status/static/js/2.3f2166db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96116942d96c36c553da",
+    "url": "/renault-order-status/static/js/main.695ca308.chunk.js"
+  },
+  {
+    "revision": "4a7199fc04147d301d3e",
+    "url": "/renault-order-status/static/js/runtime-main.7ea89470.js"
+  },
+  {
+    "revision": "c393476d000a957f0f28131e85a72a41",
+    "url": "/renault-order-status/static/media/D17.c393476d.png"
+  },
+  {
+    "revision": "da680879cd10c118d6640aad0740f207",
+    "url": "/renault-order-status/static/media/D69.da680879.png"
+  },
+  {
+    "revision": "649925321ae99b6626e4a5511a62beb9",
+    "url": "/renault-order-status/static/media/KAD.64992532.png"
+  },
+  {
+    "revision": "8b1434043842c59dd637e51612b7ac49",
+    "url": "/renault-order-status/static/media/NPI.8b143404.png"
+  },
+  {
+    "revision": "ddd1999f2f92132d27d7fdb6bee5292b",
+    "url": "/renault-order-status/static/media/RQH.ddd1999f.png"
+  },
+  {
+    "revision": "6ffc23b3aefeb00f002f17a812dba2f1",
+    "url": "/renault-order-status/static/media/XTK.6ffc23b3.png"
+  },
+  {
+    "revision": "ca4c1e4c31f09c6e0d363ee8f7e94d83",
+    "url": "/renault-order-status/static/media/XTL.ca4c1e4c.png"
+  },
+  {
+    "revision": "2d467ac11142d816b6f002d09d679169",
+    "url": "/renault-order-status/static/media/XTM.2d467ac1.png"
+  },
+  {
+    "revision": "9ac9877960533888252677230983fd53",
+    "url": "/renault-order-status/static/media/XTN.9ac98779.png"
+  },
+  {
+    "revision": "a4f80941a8c7be224279416dad409579",
+    "url": "/renault-order-status/static/media/XTP.a4f80941.png"
+  },
+  {
+    "revision": "1674ec6172a19666e4d652fb76b59577",
+    "url": "/renault-order-status/static/media/XTR.1674ec61.png"
+  },
+  {
+    "revision": "05aeb129c8be1043d8a8f96ff6efbabb",
+    "url": "/renault-order-status/static/media/XUE.05aeb129.png"
+  },
+  {
+    "revision": "002ac3802893b25ba44bae28967305e2",
+    "url": "/renault-order-status/static/media/XVQ.002ac380.png"
+  },
+  {
+    "revision": "6955fcb6636592e74004048791801cac",
+    "url": "/renault-order-status/static/media/YNL.6955fcb6.png"
+  },
+  {
+    "revision": "fdf20c066bed42dcfc63c0a3983d7ead",
+    "url": "/renault-order-status/static/media/YNN.fdf20c06.png"
+  },
+  {
+    "revision": "070b4158b3cdf2356af3d8515b032e2d",
+    "url": "/renault-order-status/static/media/YQX.070b4158.png"
+  },
+  {
+    "revision": "ddfee3aea982a9f1bc38629f062a7817",
+    "url": "/renault-order-status/static/media/YQY.ddfee3ae.png"
+  },
+  {
+    "revision": "8314be9479ef6008817e9aeb9e050c64",
+    "url": "/renault-order-status/static/media/YQZ.8314be94.png"
+  },
+  {
+    "revision": "ca8584ced4a8434266a4aa93d38672b6",
+    "url": "/renault-order-status/static/media/YRA.ca8584ce.png"
+  },
+  {
+    "revision": "203f12100c92f516392597b16568ba1e",
+    "url": "/renault-order-status/static/media/YRB.203f1210.png"
+  },
+  {
+    "revision": "0a26def11ef62e276e0ea99c13562b33",
+    "url": "/renault-order-status/static/media/YRC.0a26def1.png"
+  }
+]);
