3e28e250303e5059e42c232165e2ced8124d3313
Alexander.G.Fedotov <alexander.g.fedotov@kaspersky.com>
2020-09-08 18:27:04 +0300

    Added mock data so that the app can work independently

---
+++ src/mocks/red-white-car.ts
@@ -0,0 +1,6 @@
+export default {
+  loading: false,
+  carData: [{ status: 10 }, { color: 'YQZ', model: 'KAPTUR', version: 'Style_TCe150_4x4_CVT_X-Tronic' }, { plandt: '2020-08-23T20:38:27.399345+00:00', realdt: '2020-08-23T20:38:27.12661+00:00' }, {
+    deldt: '2020-08-24T00:00:00+00:00', effdt: null, indt: '2020-08-05T00:00:00+00:00', madcdt: '2020-07-30T11:55:50+00:00', miledt: '2020-07-30T12:03:01+00:00', orderdt: '2020-08-17T00:00:00+00:00', outdt: '2020-08-05T06:32:33+00:00', pickdt: null,
+  }],
+};
