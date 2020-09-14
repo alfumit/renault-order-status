f3baeed6691e867b04629b497c689727ddeddb5f
Alexander.G.Fedotov <alexander.g.fedotov@kaspersky.com>
2020-09-14 17:49:46 +0300

    fix: status adjustments

--- src/components/CarCard/CarStatus/CarStatus.tsx
+++ src/components/CarCard/CarStatus/CarStatus.tsx
@@ -22,22 +22,46 @@ const CarStatus = () => {
   const classes = useStyles();
   const { status } = carData[0];
   const steps = ['Order confirmed', 'Car ready', 'Car ready 2', 'On it\'s way to dealer',
-    'Delivered to dealer', 'Received by customer'/* , 'Making PTS', 'PTS ready' */];
+    'Delivered to dealer', 'Received by customer'];
+  const extraSteps = ['Making PTS', 'PTS ready'];
+  let activeStep = status;
+  let extraStep = -1;
+
+  if (status > 6) {
+    activeStep = 5;
+    extraStep = status - 7;
+  }
 
   return (
     <div>
       <Typography variant="h4" gutterBottom>Detailed status of your order:</Typography>
       <Grid container className={classes.stepsContainer} justify="center">
         {steps.map((item, index) => (
-          <Grid item xs={12} sm={6} md={4} key={item} className={index < status ? classes.passed : ''}>
+          <Grid item xs={12} sm={6} md={4} key={item} className={index < activeStep ? classes.passed : ''}>
             <div className={classes.step}>
-              {index < status && <Icon>check_circle_outline</Icon>}
-              {index >= status && <Icon>highlight_off</Icon>}
+              {index < activeStep && <Icon>check_circle_outline</Icon>}
+              {index >= activeStep && <Icon>highlight_off</Icon>}
               {` Step ${index + 1}: ${item}`}
             </div>
           </Grid>
         ))}
       </Grid>
+      {extraStep >= 0 && (
+      <>
+        <Typography variant="h6" gutterBottom>Acquiring PTS:</Typography>
+        <Grid container className={classes.stepsContainer} justify="center">
+          {extraSteps.map((item, index) => (
+            <Grid item xs={12} sm={6} md={4} key={item} className={index < extraStep ? classes.passed : ''}>
+              <div className={classes.step}>
+                {index < extraStep && <Icon>check_circle_outline</Icon>}
+                {index >= extraStep && <Icon>highlight_off</Icon>}
+                {` Step ${index + 1}: ${item}`}
+              </div>
+            </Grid>
+          ))}
+        </Grid>
+      </>
+      )}
     </div>
   );
 };
